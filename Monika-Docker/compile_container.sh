1b0ccf8dc4d568990b0cdd61c3008aaa74853e0c
Capuccino <chinodesuuu@gmail.com>
2018-01-06 13:55:06 +0000

    tidy up Monika.Clara

---
+++ Monika-Docker/compile_container.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# The reason we're doing the package install here is to keep everything in one layer for easier downloads
+# It's relatively more convinient this way
+dnf install -y \
+    curl \
+    wget \
+    gcc \
+    ffmpeg \
+    clang \
+    build-essential \
+    git \
+    glibc \
+    systemd \
+    tar \
+    which \
+    make \
+    cmake \
+    openssl-devel \
+    sudo \
+    nss_wrapper \
+    gettext \
+    nodejs
+
+# npm install yo?
+npm i -g pm2
+
+# manually install Python 3.6
+cd /usr/src && \
+   wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz && \
+   tar xzf Python-3.6.3.tgz && \
+   cd Python-3.6.3 && \
+   ./configure --enable-optimizations && \
+   make altinstall && \
+   rm -rf /usr/src/Python-3.6.3.tgz && \
+/usr/bin/python3 -V
+
+# Create user
+adduser user -u 1000 -g 0 -r -m -d /home/user/ -c "Default Application User" -l
+echo "user ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/user
+chmod 0440 /etc/sudoers.d/user
+
+#clone repo, expose Clara as app, then trim contents
+git clone https://github.com/MonikaDesu/monika --bare --depth=10 /opt/monika && \
+mkdir /opt/app && \
+rm -rf /opt/Clara
+
+# perm root awau
+chmod g+rw /opt
+chgrp root /opt
+
+# allow to run on openshift
+chown -R user:root /opt/app
+chmod -R g+rw /opt/app
+chmod -R g+rw /home/user
+find /home/user -type d -exec chmod g+x {} +
+
+# Clean up
+dnf clean all
--- Monika-Docker/container_run.sh
+++ Monika-Docker/container_run.sh
@@ -1,73 +1,7 @@
 #!/bin/sh
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < /tmp/passwd_template > /tmp/passwd
 
-# The reason we're doing the package install here is to keep everything in one layer for easier downloads
-# It's relatively more convinient this way
-dnf install -y \
-    gcc \
-    ffmpeg \
-    clang \
-    build-essential \
-    git \
-    glibc \
-    systemd \
-    tar \
-    which \
-    make \
-    cmake \
-    openssl-devel \
-    sudo \
-    nss_wrapper \
-    gettext \
-    nodejs
-
-# npm install yo?
-npm i -g pm2
-
-# manually install Python 3.6
-cd /usr/src && \
-   wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz && \
-   tar xzf Python-3.6.3.tgz && \
-   cd Python-3.6.3 && \
-   ./configure --enable-optimizations && \
-   make altinstall && \
-   rm -rf /usr/src/Python-3.6.3.tgz && \
-/usr/bin/python3 -V
-
-# Create user
-adduser user -u 1000 -g 0 -r -m -d /home/user/ -c "Default Application User" -l
-echo "user ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/user
-chmod 0440 /etc/sudoers.d/user
-
-#clone repo, expose Clara as app, then trim contents
-git clone https://github.com/MonikaDesu/monika --bare --depth=10 /opt/monika && \
-mkdir /opt/app && \
-mv /opt/monika/src/Clara/*  /opt/app/ && \
-cp /opt/monika/systemd/*.service /etc/systemd/system/ && \
-echo "
-[Unit]
-
-Description = Monika Gateway
-
-[Service]
-
-ExecStart=/usr/bin/pm2 start /opt/app/bot.js
-
-WorkingDirectory=/opt/app
-
-ExecStop= /bin/sleep 3000
-
-[Install]
-
-WantedBy=multi-user.target
-" >> /etc/systemd/system/Monika.Discord.service && \
-rm -rf /opt/Clara
-
-# perm root awau
-chmod g+rw /opt
-chgrp root /opt
-
-# allow to run on openshift
-chown -R user:root /opt/app
-chmod -R g+rw /opt/app
-chmod -R g+rw /home/user
-find /home/user -type d -exec chmod g+x {} +
+/usr/bin/pm2 start /opt/app/bot.js -e /home/user/.clara/err.log -o /home/user/.clara/out.log && \
+/usr/bin/pm2 logs bot
